41643b37b4e2f4903eef43dacce04b6223319a6f
ahmedelmoselhy98 <ahmedelmoselhy98@gmail.com>
2019-06-18 15:13:41 +0200

    #commit_3

--- app/src/main/java/com/OrxtraDev/TitoApp/activity/WalletActivity.java
+++ app/src/main/java/com/OrxtraDev/TitoApp/activity/WalletActivity.java
@@ -1,16 +1,48 @@
 package com.OrxtraDev.TitoApp.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 
 import com.OrxtraDev.TitoApp.R;
+import com.OrxtraDev.TitoApp.model.WalletModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 
 public class WalletActivity extends AppCompatActivity {
+    @BindView(R.id.moneyTV)
+    TextView moneyTV;
+    @BindView(R.id.loading)
+    ProgressBar loading;
+
+    //init the firebase
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private DatabaseReference walletRef;
+    private DatabaseReference categoryRef;
+    private DatabaseReference carRef;
+
+
+    private String clientId = "";
+    private double companyPercent = 0.0;
+    private double companyMoney = 0.0;
+    private double totalPaidPrice = 0.0;
+    private double totalPrice = 0.0;
+    private double difference = 0.0;
 
 
     @Override
@@ -18,6 +50,43 @@ public class WalletActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wallet);
         ButterKnife.bind(this);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+        clientId = mFirebaseUser.getUid();
+
+        walletRef = FirebaseDatabase.getInstance().getReference().child("Wallet");
+        categoryRef = FirebaseDatabase.getInstance().getReference().child("Category");
+        carRef = FirebaseDatabase.getInstance().getReference().child("Cars");
+
+        walletRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        if (snapshot.exists()) {
+                            WalletModel model = snapshot.getValue(WalletModel.class);
+                            if (model.getClientId().equals(clientId)) {
+                                totalPaidPrice += Double.parseDouble(model.getPaidPrice());
+                                totalPrice += Double.parseDouble(model.getPrice());
+                            }
+                        }
+                    }
+                    difference = totalPaidPrice - totalPrice;
+                    loading.setVisibility(View.GONE);
+                    moneyTV.setText(difference + " EGP");
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 
 
---
+++ app/src/main/java/com/OrxtraDev/TitoApp/model/WalletModel.java
@@ -0,0 +1,64 @@
+package com.OrxtraDev.TitoApp.model;
+
+public class WalletModel {
+
+
+    private String tripId ="";
+    private String driverId  = "";
+    private String clientId = "";
+    private String price = "";
+    private String paidPrice = "";
+    private String time = "";
+
+    public WalletModel() {
+
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getTripId() {
+        return tripId;
+    }
+
+    public void setTripId(String id) {
+        this.tripId = id;
+    }
+
+    public String getDriverId() {
+        return driverId;
+    }
+
+    public void setDriverId(String driverId) {
+        this.driverId = driverId;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getPaidPrice() {
+        return paidPrice;
+    }
+
+    public void setPaidPrice(String paidPrice) {
+        this.paidPrice = paidPrice;
+    }
+}
